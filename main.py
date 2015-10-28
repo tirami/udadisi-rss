a3ff6ef304f6ceae706e8649249b4252df78a275
David Morrison <me@davemor.com>
2015-10-28 22:04:25 +0000

    Set the miner function to run every 10 seconds in the background.  Close #2.

--- main.py
+++ main.py
@@ -6,10 +6,13 @@ import time
 uris = ["https://news.ycombinator.com/rss", "http://feeds.bbci.co.uk/news/technology/rss.xml?edition=uk"]
 miner_name = "RSS miner one: BBC and Hacker News"
 engine_uri = "http://localhost:4000/"
+interval = 10 # in seconds
 
 def run_miner_every_interval():
-    run_miner(miner_name, uris, engine_uri)
-    time.sleep(100000)
+    while True:
+        run_miner(miner_name, uris, engine_uri)
+        time.sleep(interval)
 
 
-miner_thread = threading.Timer(run_miner_every_interval)
+miner_thread = threading.Thread(target=run_miner_every_interval)
+miner_thread.start()
